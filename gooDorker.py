463e0beeae731be75389a4c59fa099fd3191e33e
403 - Forbidden <60777262+G0urmetD@users.noreply.github.com>
2024-04-23 11:54:51 +0200

    Update gooDorker.py

--- gooDorker.py
+++ gooDorker.py
@@ -2,7 +2,7 @@ import argparse
 from googlesearch import search as google_search
 from colorama import init, Fore, Style
 
-# Initialisiere Colorama für Windows
+# Initialize Colorama for Windows
 init()
 
 # ASCII Banner
@@ -20,11 +20,11 @@ ascii_banner = f"""
 {Fore.RED}version: 1.0                                          
 """
 
-# Funktion zur Generierung der Google Dorks
+# Function to generate Google Dorks
 def generate_dorks(domain):
     dorks = []
 
-    # Suche nach Subdomains
+    # Search for subdomains
     subdomain_dorks = [
         f'site:*.{domain} -www',
         f'site:*.*.{domain} -www',
@@ -32,7 +32,7 @@ def generate_dorks(domain):
     ]
     dorks.extend(subdomain_dorks)
 
-    # Suche nach typischen Dateiendungen
+    # Search for typical file extensions
     file_dorks = [
         f'site:{domain} (filetype:pdf OR filetype:doc OR filetype:docx OR filetype:xls OR filetype:xlsx OR filetype:ppt OR filetype:txt OR filetype:odt OR filetype:pps OR filetype:xml)',
         f'site:{domain} (filetype:asp OR filetype:aspx OR filetype:php OR filetype:py OR filetype:bak OR filetype:conf OR filetype:eml OR filetype:log OR filetype:ps1 OR filetype:reg OR filetype:sql)'
@@ -41,26 +41,26 @@ def generate_dorks(domain):
 
     return dorks
 
-# Funktion zur Durchführung der Google-Suche und Ausgabe der Ergebnisse in der Kommandozeile
+# Function to perform Google search and output results in the command line
 def perform_search(domain, rate_limit):
     dorks = generate_dorks(domain)
 
-    # Suchanfragen durchführen
+    # Perform searches
     for dork in dorks:
         print(f"{Fore.GREEN}[*] Searching for: {dork}")
         for url in google_search(dork, num=10, stop=10, pause=rate_limit):
             print(f"{Fore.YELLOW}{url}")
-        print()  # Leere Zeile zwischen den Suchergebnissen
+        print()  # Empty line between search results
 
 if __name__ == "__main__":
-    # ASCII Banner ausgeben
+    # Print ASCII banner
     print(ascii_banner)
 
-    # Argumente parsen
+    # Parse arguments
     parser = argparse.ArgumentParser(description='Google Dorking Tool')
     parser.add_argument('-d', '--domain', type=str, required=True, help='Domain to search for')
     parser.add_argument('-r', '--rate-limit', type=float, default=5.0, help='Rate limit in seconds between searches')
     args = parser.parse_args()
 
-    # Suchen durchführen und Ergebnisse in Kommandozeile ausgeben
+    # Perform searches and output results in command line
     perform_search(args.domain, args.rate_limit)
