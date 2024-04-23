aab342782caa65db99b320b2a0d70afa7a43a76e
403 - Forbidden <60777262+G0urmetD@users.noreply.github.com>
2024-04-23 14:12:58 +0200

    Update gooDorker.py
    adding more search queries

--- gooDorker.py
+++ gooDorker.py
@@ -48,6 +48,100 @@ def generate_dorks(domain):
     ]
     dorks.extend(sql_dorks)
 
+   # Search for pages that have login/admin/password in title
+    pages_dorks = [
+        f'site:{domain} inurl:"login"',
+        f'site:{domain} inurl:"admin"',
+        f'site:{domain} inurl:"password"'
+    ]
+    dorks.extend(pages_dorks)
+
+    # Search for pages that have login/admin/password in URL
+    urlpages_dorks = [
+        f'site:{domain} intitle:"login"',
+        f'site:{domain} intitle:"admin"',
+        f'site:{domain} intitle:"password"'
+    ]
+    dorks.extend(urlpages_dorks)
+
+    # Search for information out of sql database files containing username password
+    sqldatabasefiles_dorks = [
+        f'site:{domain} filetype:sql intext:username password'
+    ]
+    dorks.extend(sqldatabasefiles_dorks)
+
+    # Search for security cameras access
+    securitycams_dorks = [
+        f'site:{domain} inurl:/view/viewer_index.shtml'
+    ]
+    dorks.extend(securitycams_dorks)
+
+    # Search for phpMyAdmin panels
+    phpMyAdmin_dorks = [
+        f'site:{domain} inurl:"/phpmyadmin/"'
+    ]
+    dorks.extend(phpMyAdmin_dorks)
+
+    # Search for cacti device monitoring panels
+    cactimonitoring_dorks = [
+        f'site:{domain} inurl:"/cacti/"'
+    ]
+    dorks.extend(cactimonitoring_dorks)
+
+    # Search for Cisco WebVPN services
+    ciscowebvpn_dorks = [
+        f'site:{domain} inurl:webvpn.html'
+    ]
+    dorks.extend(ciscowebvpn_dorks)
+
+    # Search for .htpasswd files
+    htpasswdfiles_dorks = [
+        f'site:{domain} inurl:.htpasswd'
+    ]
+    dorks.extend(htpasswdfiles_dorks)
+
+    # Search for PHP info pages
+    phpinfopages_dorks = [
+        f'site:{domain} inurl:"/phpinfo.php"'
+    ]
+    dorks.extend(phpinfopages_dorks)
+
+    # Search for apache server status pages
+    apacheserverstatus_dorks = [
+        f'site:{domain} inurl:"server-status"'
+    ]
+    dorks.extend(apacheserverstatus_dorks)
+
+    # Search for Jenkins script consoles
+    jenkinsscriptconsole_dorks = [
+        f'site:{domain} inurl:"/jenkins/script"'
+    ]
+    dorks.extend(jenkinsscriptconsole_dorks)
+
+    # Search for zabbix monitoring software
+    zabbixmonitoring_dorks = [
+        f'site:{domain} inurl:"/zabbix/"'
+    ]
+    dorks.extend(zabbixmonitoring_dorks)
+
+    # Search for wp-admin panel
+    wpadminpanel_dorks = [
+        f'site:{domain} intitle:"index of" "wp-admin"'
+    ]
+    dorks.extend(wpadminpanel_dorks)
+
+    # Search for WordPress configuration files
+    wpconfigurationfiles_dorks = [
+        f'site:{domain} ext:conf inurl:wp-'
+    ]
+    dorks.extend(wpconfigurationfiles_dorks)
+
+    # Search for ftp addresses
+    ftpaddresses_dorks = [
+        f'site:{domain} inurl:ftp://'
+    ]
+    dorks.extend(ftpaddresses_dorks)
+
     return dorks
 
 # Function to perform Google search and output results in the command line
